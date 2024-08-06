a05bfd1fd53b89dcf070f1a3ab5e9bfa4a6d2c41
z-qine <zqine@localhost.localdomain>
2024-08-06 19:33:21 +0300

    implemente task 13

--- mobile/abeselom dejene/first_application/test/helpers/helpers_test.dart
+++ mobile/abeselom dejene/first_application/test/helpers/helpers_test.dart
@@ -1,9 +1,16 @@
 import 'package:first_application/features/product/data/data_sources/remote_data_source.dart';
 import 'package:first_application/features/product/domain/repositories/product_repository.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:mockito/annotations.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
-@GenerateMocks([ProductRepository, ProductRemoteDataSource, SharedPreferences],
-    customMocks: [MockSpec<http.Client>(as: #MockHttpClient)])
+@GenerateMocks([
+  ProductRepository,
+  ProductRemoteDataSource,
+  SharedPreferences,
+  InternetConnectionChecker
+], customMocks: [
+  MockSpec<http.Client>(as: #MockHttpClient)
+])
 void main() {}
